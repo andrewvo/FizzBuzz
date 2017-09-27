6f3db23f2988598554d5f1c217d515dd48b74118
Andrew Vo <andrewvo91@googlemail.com>
2017-09-27 23:05:30 +0100

    Inital code entered.

--- FizzBuzz/FizzBuzz.App/GetOutput.cs
+++ FizzBuzz/FizzBuzz.App/GetOutput.cs
@@ -6,7 +6,22 @@ namespace FizzBuzz.App
     {
         public string Get(int value)
         {
-            throw new NotImplementedException();
+            if (((value % 3) == 0) && ((value % 5) == 0))   //The FizzBuzz if Condition is implemented first to ensure no conflicts with
+            {                                               // the "Fizz" and "buzz" if statements.  
+                return "FizzBuzz";                          //If either of them were implemented before the "FizzBuzz" condition, "FizzBuzz"
+            }                                               //would not output as either "Fizz" or "Buzz" would take precedence in the If else statement.
+            else if ((value % 3) == 0)
+            {
+                return "Fizz";
+            }
+            else if ((value % 5) == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return value.ToString();
+            }
         }
     }
 }
--- FizzBuzz/FizzBuzz.App/Program.cs
+++ FizzBuzz/FizzBuzz.App/Program.cs
@@ -6,12 +6,53 @@ namespace FizzBuzz.App
     {
         private static void Main(string[] args)
         {
+            bool inputsCorrect = false;
+            int intLower =0;
+            int intUpper =0;
+            string lower = "";
+            string upper= "";
+            while (inputsCorrect == false)
+            {
+                while (inputsCorrect == false)
+                {
+                    Console.Write("Please enter the lower bound:");
+                    lower = Console.ReadLine();
+                    Console.Write("Please enter the upper bound:");
+                    upper = Console.ReadLine();
+
+
+
+                    if (Int32.TryParse(lower,out intLower) == false)
+                    {
+                        Console.WriteLine("Lower bound entered is not an integer, please try again");
+                    }
+                    else if (Int32.TryParse(upper, out intUpper) == false)
+                    {
+                        Console.WriteLine("Upper bound entered is not an integer, please try again");
+                    }
+                    else if (intLower > intUpper)
+                    {
+                        Console.WriteLine("Lower bound is higher than upper bound, please try again");
+                    }
+                    else
+                    {
+                        inputsCorrect = true;
+                    }
+                }
+            }
+
+
+
+
             IGetOutput output = new GetOutput();
 
-            for (var i = 0; i < 100; i++)
+            for (var i = intLower; i <= intUpper; i++)
             {
-                Console.WriteLine(output.Get(i));
+                    Console.WriteLine(output.Get(i));
             }
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to close the console");
+            Console.ReadKey();  //Used to keep console window open until enter key has been pressed
         }
     }
 }
